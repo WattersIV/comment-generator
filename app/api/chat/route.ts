bf12c312ece1c9c5f69536f951ac55fa1357563e
Bill Watters <billywatters13@gmail.com>
2024-06-06 16:20:54 -0400

    Adjust learning skill comment to have a min char length when gpt adjusts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -8,22 +8,26 @@ export async function POST(req: Request) {
   try {
     const { text, activeTab } = await req.json();
 
-    let idealCharacters;
-    let maxCharacters;
+    let prompt;
     switch (activeTab) {
-      case 'learning skills':
-        idealCharacters = 1700;
-        maxCharacters = 2000;
+      case 'learning skills': {
+        const minCharacters = 1600;
+        const maxCharacters = 2000;
+        prompt = `Improve this learning skill comment. Make the feedback concise using a professional tone. Make the new comment max ${maxCharacters}, min ${minCharacters} characters. Don't include anything other than then new comment in the response. Heres the comment: ${text}`;
         break;
-      default:
-        idealCharacters = 900;
-        maxCharacters = 1300;
+      }
+      default: {
+        const idealCharacters = 900;
+        const maxCharacters = 1300;
+        prompt = `Improve this comment. Make the feedback concise using a professional tone. Make the new comment max ${maxCharacters} characters and ideally ${idealCharacters} characters. Don't include anything other than then new comment in the response. Heres the comment: ${text}`;
+        break;
+      }
     }
 
     const result = await generateText({
       model: openai('gpt-3.5-turbo-16k'),
       system: 'You are an Ontario, Canada grade 5 teacher refining a report card comment for a student. Be professional and encouraging. *N* is a students name. *H*self is himself/herself. *R* is his/her. *P* is he/she. *H* is him/her. Use these as placeholders.',
-      prompt: `Improve this comment. Make the feedback concise using a professional tone. Make the new comment max ${maxCharacters} characters and ideally ${idealCharacters} characters. Don't include anything other than then new comment in the response. Heres the comment: ${text}`,
+      prompt,
     });
   
     return new Response(result.text, { status: 200 });
